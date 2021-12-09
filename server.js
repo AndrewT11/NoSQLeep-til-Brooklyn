b539a5422cad43966521ba155900e62b6ac77457
Andrew Tran <tranandrew@gmail.com>
2021-12-09 16:21:34 -0600

    deployed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //mongoose connecting to database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WorkoutDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
